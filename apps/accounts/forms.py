58cbd6a71342128342e1cce60104d67081f2280f
Adanhassan790 <adanhassanadigala@gmail.com>
2026-06-02 11:34:30 +0300

    Redesign register and contact pages

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -12,6 +12,24 @@ class ClientRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        field_placeholders = {
+            'first_name': 'Enter your first name',
+            'last_name': 'Enter your last name',
+            'email': 'name@company.com',
+            'username': 'Choose a username',
+            'password1': 'Create a strong password',
+            'password2': 'Repeat your password',
+        }
+
+        for field_name, field in self.fields.items():
+            css_classes = field.widget.attrs.get('class', '')
+            field.widget.attrs['class'] = (css_classes + ' form-control').strip()
+            field.widget.attrs['placeholder'] = field_placeholders.get(field_name, '')
+            field.widget.attrs['autocomplete'] = 'off'
     
     def clean_email(self):
         email = self.cleaned_data.get('email')
