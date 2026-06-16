b949f402a8d29e70a40a8575803319906cba9d35
Adanhassan790 <adanhassanadigala@gmail.com>
2026-06-16 22:31:04 +0300

    Add EMAIL_TIMEOUT to prevent forms hanging when SMTP connection is slow/blocked

--- config/settings.py
+++ config/settings.py
@@ -219,6 +219,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_TIMEOUT = config('EMAIL_TIMEOUT', default=10, cast=int)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='tax@hsconsulting.co.ke')
 OWNER_EMAIL = config('OWNER_EMAIL', default='hsconsultingke@gmail.com')
 
